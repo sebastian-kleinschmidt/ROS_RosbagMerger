f0b3768fbd1e08bb14cf08aedb8af31c3e4d2999
Sebastian Kleinschmidt <kleinschmidt@rts.uni-hannover.de>
2019-05-20 16:41:26 +0200

    feat: extracts parameters of each abg. Output start and end time of the output bags based in the input bags

--- merge_bags.py
+++ merge_bags.py
@@ -1,13 +1,38 @@
 import sys
 import rosbag
 import rospy
+import glob
 
 def merge_bags(output_name, folders):
+    output_start_time = -1
+    output_end_time = -1
     #Check all subdirectories of folder to find all rosbags
+    for folder in folders:
+        files = glob.glob(folder+"*.bag")
+        #List all bags
+        for file in files:
+            #Check Timestamps and list start and end time
+            with rosbag.Bag(file, 'a') as bag:
+                if(bag.get_start_time() < output_start_time or output_start_time==-1):
+                    output_start_time = bag.get_start_time()
+                if(bag.get_end_time() > output_end_time or output_end_time==-1):
+                    output_end_time = bag.get_end_time()
+                #Save name, start, end and duration of current bag
+                #print(file)
+                #print("Start Time:")
+                #print(bag.get_start_time())
+                #print(rospy.Time(bag.get_start_time()))
+                #print("End Time:")
+                #print(rospy.Time(bag.get_end_time()))
+                #print("Duration:")
+                #print(rospy.Time(bag.get_end_time())-rospy.Time(bag.get_start_time()))
     
-    #List all bags
+    #Sort all bags by starttime
     
-    #Check Timestamps and list start and end time
+    #Output some useful parameters on the measurement series
+    duration = output_end_time-output_start_time
+    print("Generating new bag in time interval from " + str(output_start_time) + " to " + str(output_end_time))
+    print("the duration will be: " + str(duration) + " seconds")
     
     #Open rosbags with intersecting time intervals
     
