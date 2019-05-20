440063b0e2691dcb788cbddc1e0a30c828ef560f
Sebastian Kleinschmidt <kleinschmidt@rts.uni-hannover.de>
2019-05-20 15:31:18 +0200

    doc: adds prototype structure for module

---
+++ merge_bags.py
@@ -0,0 +1,10 @@
+def merge_bags(folder_list, output_name):
+    #List all bags
+    
+    #Check Timestamps and list start and end time
+    
+    #Open rosbags with intersecting time intervals
+    
+    #Merge bags to new bag-file
+    
+    #Save final bag-file
