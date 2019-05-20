fe53f4e7cc0112c4f98e4e75c9d47665075230e9
Sebastian Kleinschmidt <kleinschmidt@rts.uni-hannover.de>
2019-05-20 15:39:38 +0200

    feat: add parameter injection during function call and output message in case of failure or success

--- merge_bags.py
+++ merge_bags.py
@@ -1,4 +1,8 @@
-def merge_bags(folder_list, output_name):
+import sys
+
+def merge_bags(folder, output_name):
+    #Check all subdirectories of folder to find all rosbags
+    
     #List all bags
     
     #Check Timestamps and list start and end time
@@ -8,3 +12,11 @@ def merge_bags(folder_list, output_name):
     #Merge bags to new bag-file
     
     #Save final bag-file
+    return True
+    
+
+print("Try to merge all rosbags in "+ sys.argv[1] +"...")
+if(merge_bags(sys.argv[1],sys.argv[2])):
+    print("Success: New Rosbag has successfully been generated as: "+ sys.argv[2])
+else:
+    print("FAILED: New Rosbag could not be generated!")
